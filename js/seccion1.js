88b2fdd35bcb24bbb100dd70a98cb1a0158ddeda
majo <mj.ocampov@uniandes.edu.co>
2020-08-24 18:36:36 -0500

    draft

---
+++ js/seccion1.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+
+$.ajax({
+  url:"./data/cities.csv",
+  dataType:"text",
+  success:function(data)
+  {
+    var students_data = data.split(/\r?\n|\r/);
+    var table_data = '<table class="table table-bordered table striped">';
+    for(var i=0; i < students_data.length; i++)
+    {
+      var cell_data = students_data[i].split(",");
+      table_data += '<tr>';
+      for (var j = 0; j < cell_data.length; j++) 
+      {
+        if (i === 0) 
+        {
+          table_data += '<th>' + cell_data[j] + '</th>';
+        }
+
+        else 
+        {
+          table_data += '<td>' + cell_data[j] + '</td>';
+        }
+      }
+      table_data += '</tr>';
+    }
+    table_data += '</table>';
+    $('#students_table').html(table_data);
+  }
+});
+}
