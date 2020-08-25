a290efb4a72d789ae87199bba9dd1cd64e440219
majo <mj.ocampov@uniandes.edu.co>
2020-08-24 20:01:26 -0500

    finish

--- js/seccion1.js
+++ js/seccion1.js
@@ -1,55 +1,51 @@
 window.onload = function () {
 
-d3.csv("data/cities.csv").then(function(data) {
+d3.csv("data/seccion1.csv").then(function(data) {
   console.log(data[0]);
-  data.sort((a, b) => (a.population > b.population) ? 1 : -1);
+  data.sort((a, b) => (a.puntos < b.puntos) ? 1 : -1);
 
   var table_data = '<table class="table table-bordered table striped">';
   table_data += '<tr>';
-  table_data += '<th>' + "Titulo" + '</th>';
+  table_data += '<th>' + "Apellidos" + '</th>';
+  table_data += '<th>' + "Nombre" + '</th>';
+  table_data += '<th>' + "Puntos" + '</th>';
   table_data += '</tr>';
   for(var i=0; i < data.length; i++)
   {
-    table_data += '<tr>';
-    table_data += '<td>' + data[i].city + '</td>';
-    table_data += '<td>' + data[i].landarea + '</td>';
-    table_data += '<td>' + data[i].population + '</td>';
-    table_data += '<td>' + data[i].state + '</td>';
-    table_data += '</tr>';
+    if (i === 0) 
+    {
+      table_data += '<tr class ="first">';
+      table_data += '<td>' + data[i].apellidos + '</td>';
+      table_data += '<td>' + data[i].nombre + '</td>';
+      table_data += '<td>' + data[i].puntos + '</td>';
+      table_data += '</tr>';
+    }
+    if (i === 1) 
+    {
+      table_data += '<tr class ="second">';
+      table_data += '<td>' + data[i].apellidos + '</td>';
+      table_data += '<td>' + data[i].nombre + '</td>';
+      table_data += '<td>' + data[i].puntos + '</td>';
+      table_data += '</tr>';
+    }
+    if (i === 2) 
+    {
+      table_data += '<tr class ="third">';
+      table_data += '<td>' + data[i].apellidos + '</td>';
+      table_data += '<td>' + data[i].nombre + '</td>';
+      table_data += '<td>' + data[i].puntos + '</td>';
+      table_data += '</tr>';
+    }
+    else
+    {
+      table_data += '<tr>';
+      table_data += '<td>' + data[i].apellidos + '</td>';
+      table_data += '<td>' + data[i].nombre + '</td>';
+      table_data += '<td>' + data[i].puntos + '</td>';
+      table_data += '</tr>';
+    }
   }
   table_data += '</table>';
   $('#students_table').html(table_data);
 });
-
-
-
-/**$.ajax({
-  url:"https://github.com/mjocampov/infracomp202020/blob/master/data/cities.csv",
-  dataType:"text",
-  success:function(data)
-  {
-    var students_data = data.split(/\r?\n|\r/);
-    var table_data = '<table class="table table-bordered table striped">';
-    for(var i=0; i < students_data.length; i++)
-    {
-      var cell_data = students_data[i].split(",");
-      table_data += '<tr>';
-      for (var j = 0; j < cell_data.length; j++) 
-      {
-        if (i === 0) 
-        {
-          table_data += '<th>' + cell_data[j] + '</th>';
-        }
-
-        else 
-        {
-          table_data += '<td>' + cell_data[j] + '</td>';
-        }
-      }
-      table_data += '</tr>';
-    }
-    table_data += '</table>';
-    $('#students_table').html(table_data);
-  }
-});**/
 }
